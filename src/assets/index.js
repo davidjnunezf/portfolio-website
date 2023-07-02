196c3f5ba08877725e1515b361c49b819ab54146
David Nunez <43526645+davidjnunezf@users.noreply.github.com>
2023-07-02 16:20:20 -0400

    update casing

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,7 +20,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import lit from "./tech/lit.png";
+import lit from "./tech/LIT.png";
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
